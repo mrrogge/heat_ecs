e31945ba9678fb7a3c6be0d566951806bdcb745e
Matt Rogge <5900418+mrrogge@users.noreply.github.com>
2022-01-13 17:20:16 -0600

    rewrite tests

--- test/Main.hx
+++ test/Main.hx
@@ -15,94 +15,91 @@ enum TestEnum1 {
 
 class Main extends buddy.SingleSuite {
     public function new() {
-        describe("Worlds provide new, unique int IDs with getId() (starting with 1)", {
-            describe("given a world", {
-                var world = new heat.ecs.World();
 
-                it("returns 1 for the first ID", {
-                    var expected:heat.ecs.EntityId = 1;
-                    world.getId().should.equal(expected);
+        var comMapGroup = new heat.ecs.ComMapGroup2(
+            new heat.ecs.ComMap<TestClass1>(), 
+            new heat.ecs.ComMap<TestClass2>());
+
+        describe("ComQuery instances return all entity IDs with matching coms", {
+            describe("given 2 com maps each with coms for the same 2 entities", {
+                var map0 = new Map<heat.ecs.EntityId, TestClass1>();
+                var map1 = new Map<heat.ecs.EntityId, TestClass2>();
+                var id1:heat.ecs.EntityId = 1;
+                var id2:heat.ecs.EntityId = 2;
+                map0[id1] = new TestClass1();
+                map0[id2] = new TestClass1();
+                map1[id1] = new TestClass2();
+                map1[id2] = new TestClass2();
+
+                describe("and a ComQuery requiring those two coms", {
+                    var query = new heat.ecs.ComQuery()
+                        .with(map0)
+                        .with(map1);
+                    
+                    it("that ComQuery should return both IDs", {
+                        query.run();
+                        query.result.should.containAll([id1, id2]);
+                    });
                 });
+            });
 
-                it("returns 2 for the next ID", {
-                    var expected:heat.ecs.EntityId = 2;
-                    world.getId().should.equal(expected);
+            describe("given a com map with coms for two entities", {
+                var map0 = new Map<heat.ecs.EntityId, TestClass1>();
+                var id1:heat.ecs.EntityId = 1;
+                var id2:heat.ecs.EntityId = 2;
+                map0[id1] = new TestClass1();
+                map0[id2] = new TestClass1();
+
+                describe("and a com map with a com added for only one of those entities", {
+                    var map1 = new Map<heat.ecs.EntityId, TestClass2>();
+                    map1[id1] = new TestClass2();
+
+                    describe("and a ComQuery requiring both coms", {
+                        var query = new heat.ecs.ComQuery()
+                            .with(map0)
+                            .with(map1);
+
+                        it("that ComQuery should return the ID with both coms", {
+                            query.run();
+                            query.result.should.contain(id1);
+                        });
+
+                        it("that ComQuery should not return the ID missing a com", {
+                            query.run();
+                            query.result.should.not.contain(id2);
+                        });
+                    });
                 });
             });
         });
 
-        describe("Worlds store and recall components for an ID", {
-            describe("given a world", {
-                var world = new heat.ecs.World();
-
-                describe("and a stored class component", {
-                    var testCom = new TestClass1();
-                    var testId = 1;
-                    world.setClassCom(TestClass1, testId, testCom);
-
-                    it("returns the stored component for that ID", {
-                        world.getClassCom(TestClass1, testId).should.equal(haxe.ds.Option.Some(testCom));
-                    });
-                });
-
-                describe("and a stored enum component", {
-                    var testCom:TestEnum1 = TEST;
-                    var testId = 1;
-                    world.setEnumCom(TestEnum1, testId, testCom);
-
-                    it("returns the stored component for that ID", {
-                        world.getEnumCom(TestEnum1, testId).should.equal(haxe.ds.Option.Some(testCom));
-                    });
-                });
-            });
-        });
-
-        describe("Queries return IDs that meet all criteria", {
-            describe("given a world with two entities, each with two components", {
-                var world = new heat.ecs.World();
+        describe('ComQuery instances should exclude entity IDs containing "without" coms', {
+            describe("given a com map with coms for two entities", {
+                var map0 = new Map<heat.ecs.EntityId, TestClass1>();
                 var id1:heat.ecs.EntityId = 1;
                 var id2:heat.ecs.EntityId = 2;
-                world.setClassCom(TestClass1, id1, new TestClass1());
-                world.setClassCom(TestClass2, id1, new TestClass2());
-                world.setClassCom(TestClass1, id2, new TestClass1());
-                world.setClassCom(TestClass2, id2, new TestClass2());
-                
-                it("a query for those classes returns both IDs", {
-                    var query = new heat.ecs.EntityQuery(world)
-                    .with(TestClass1)
-                    .with(TestClass2);
-                    query.run();
-                    query.result.should.containAll([id1, id2]);
-                });
-            });
+                map0[id1] = new TestClass1();
+                map0[id2] = new TestClass1();
 
-            describe("given a world with two entities, one with two components, and another with only one", {
-                var world = new heat.ecs.World();
-                var id1:heat.ecs.EntityId = 1;
-                var id2:heat.ecs.EntityId = 2;
-                world.setClassCom(TestClass1, id1, new TestClass1());
-                world.setClassCom(TestClass2, id1, new TestClass2());
-                world.setClassCom(TestClass1, id2, new TestClass1());
+                describe("and a com map with a com added for only one of those entities", {
+                    var map1 = new Map<heat.ecs.EntityId, TestClass2>();
+                    map1[id1] = new TestClass2();
 
-                it("a query for those classes returns only the first ID", {
-                    var query = new heat.ecs.EntityQuery(world)
-                    .with(TestClass1)
-                    .with(TestClass2);
-                    query.run();
-                    query.result.should.containExactly([id1]);
-                });
-            });
+                    describe("and a ComQuery that requires the first com and excludes the second com", {
+                        var query = new heat.ecs.ComQuery()
+                            .with(map0)
+                            .without(map1);
+                        
+                        it("that ComQuery should not return the ID with the excluded com", {
+                            query.run();
+                            query.result.should.not.contain(id1);
+                        });
 
-            describe("given a world with a component for some entity", {
-                var world = new heat.ecs.World();
-                var id1:heat.ecs.EntityId = 1;
-                world.setClassCom(TestClass1, id1, new TestClass1());
-
-                it("a query for a different component should not return that ID", {
-                    var query = new heat.ecs.EntityQuery(world)
-                    .with(TestClass2);
-                    query.run();
-                    query.result.should.not.contain(id1);
+                        it("that ComQuery should return the ID without the excluded com", {
+                            query.run();
+                            query.result.should.contain(id2);
+                        });
+                    });
                 });
             });
         });
