ddbbef376201287c52ecb66e796b0db910efaefb
Matt Rogge <5900418+mrrogge@users.noreply.github.com>
2022-01-13 17:20:07 -0600

    add new ComQuery class

---
+++ src/heat/ecs/ComQuery.hx
@@ -0,0 +1,70 @@
+package heat.ecs;
+
+class ComQuery {
+    public var result(default, null) = new Array<EntityId>();
+
+    var withMaps =  new Array<Map<EntityId, Dynamic>>();
+    var withoutMaps = new Array<Map<EntityId, Dynamic>>();
+
+    public function new() {
+
+    }
+
+    public function with(comMap:Map<EntityId, Dynamic>):ComQuery {
+        for (map in withMaps) {
+            if (comMap == map) return this;
+        }
+        withMaps.push(comMap);
+        return this;
+    }
+
+    public function without(comMap:Map<EntityId, Dynamic>):ComQuery {
+        for (map in withoutMaps) {
+            if (comMap == map) return this;
+        }
+        withoutMaps.push(comMap);
+        return this;
+    }
+
+    public function run():ComQuery {
+        while (result.length > 0) result.pop();
+        var firstMap = withMaps[0];
+        if (firstMap == null) return this;
+        for (id => _ in firstMap) {
+            var hasAllRequiredComs = true;
+            for (map in withMaps) {
+                if (map == firstMap) continue;
+                if (!map.exists(id)) {
+                    hasAllRequiredComs = false;
+                    break;
+                }
+            }
+            if (!hasAllRequiredComs) continue;
+            var hasNoDisallowedComs = true;
+            for (map in withoutMaps) {
+                if (map.exists(id)) {
+                    hasNoDisallowedComs = false;
+                    break;
+                }
+            }
+            if (!hasNoDisallowedComs) continue;
+            result.push(id);
+        }
+        return this;
+    }
+
+    public inline function iter():Iterator<EntityId> {
+        run();
+        return result.iterator();
+    }
+
+    public function checkId(id:EntityId):Bool {
+        for (map in withMaps) {
+            if (!map.exists(id)) return false;
+        }
+        for (map  in withoutMaps) {
+            if (map.exists(id)) return false;
+        }
+        return true;
+    }
+}
